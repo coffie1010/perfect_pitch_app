cd1e93013364c2c026b010d7a852569374e55645
coffie1010 <kazunori.ishihara@gmail.com>
2025-07-01 02:09:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ playButton.addEventListener("click", async () => {
   // 🔊 音量補正
   let volumeAdjust = 0;
   switch (randomType) {
-    case "sawtooth": volumeAdjust = -13; break;
-    case "square": volumeAdjust = -13; break;
+    case "sawtooth": volumeAdjust = -10; break;
+    case "square": volumeAdjust = -10; break;
     case "triangle": volumeAdjust = -2; break;
     case "sine": volumeAdjust = 0; break;
   }
